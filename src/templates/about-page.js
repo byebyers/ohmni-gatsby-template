22d9105af5494238821781c39c8c215c6e941188
Jacob Byers <byers.jacob@gmail.com>
2020-02-27 15:01:22 -0700

    plz again

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
-import Content, { HTMLContent } from '../components/Content'
+import Content, { HTMLContent } from '../components/content'
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
