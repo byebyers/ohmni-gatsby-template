29ca3d750014b0c69b0d7143cc19bde0dfdc3919
Jacob Byers <byers.jacob@gmail.com>
2020-02-28 20:22:16 -0700

    Adding webpack config to use sass

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -10,13 +10,11 @@ export const IndexPageTemplate = ({
   subheading,
   banner,
 }) => (
-  <div>
   <Fullban
     heading={heading}
     subheading={subheading}
     banner={banner}
   />
-  </div>
 )
 
 IndexPageTemplate.propTypes = {
