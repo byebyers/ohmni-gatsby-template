f56cbd54aaa3ad16544eada7474842bde32c0dd3
Jacob Byers <byers.jacob@gmail.com>
2020-03-02 16:35:21 -0700

    Fix: Changing name of home page to index to see if it matches the page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,4 +2,4 @@ import CMS from 'netlify-cms-app'
 
 import IndexPagePreview from './preview-templates/HomePagePreview'
 
-CMS.registerPreviewTemplate('home', IndexPagePreview)
+CMS.registerPreviewTemplate('index', IndexPagePreview)
