2e55483f40c196bbe4c4abed5ee7f18337815308
erezrokah <erezrokah@users.noreply.github.com>
2020-03-25 12:50:04 +0200

    fix: cms preview template

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,7 +99,6 @@ module.exports = {
        resolve: 'gatsby-plugin-netlify-cms',
        options: {
          modulePath: `${__dirname}/src/cms/cms.js`,
-         stylesPath: `${__dirname}/src/global.scss`
        },
      },
      `gatsby-plugin-offline`,
