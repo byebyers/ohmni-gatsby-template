fe55f2f33aecf0030aa556549beb680e78c85c76
Jacob Byers <byers.jacob@gmail.com>
2020-03-31 18:46:30 -0700

    Update: Tag Section Styling

--- src/components/tags/tag-section.js
+++ src/components/tags/tag-section.js
@@ -2,18 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import { kebabCase } from 'lodash'
+import './tag-section.scss'
 
 const TagSection = ({ tags }) => {
   return (
     <div>
       {tags && tags.length ? (
         <div style={{ marginTop: `4rem` }}>
-          <h4>Tags</h4>
           <ul className="taglist">
             {tags.map(tag => (
-              <li key={tag + `tag`}>
-                <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-              </li>
+              <div className="tag-container">
+                <li key={tag + `tag`}>
+                  <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                </li>
+              </div>
             ))}
           </ul>
         </div>
