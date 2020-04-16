e56affeee0d11fd095e27c27075c3eef6b6a26e5
Jacob Byers <byers.jacob@gmail.com>
2020-04-16 16:03:19 -0700

    Fix: Container in Author Preview

--- src/cms/preview-templates/AuthorPagePreview.js
+++ src/cms/preview-templates/AuthorPagePreview.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { AuthorPageTemplate } from '../../templates/author-page'
+import Container from '../../components/container/container'
 
 const AuthorPagePreview = ({ entry, widgetFor }) => {
   const data = entry.getIn(['data']).toJS()
   if (data) {
     return (
-      <AuthorPageTemplate
-        title={data.title}
-        description={data.description}
-        thumbnail={data.thumbnail}
-        facebook={data.facebook}
-        instagram={data.instagram}
-        linkedin={data.linkedin}
-        twitter={data.twitter}
-        inWebsite={data.inWebsite}
-        inMail={data.inMail}
-      />
+      <Container>
+        <AuthorPageTemplate
+          title={data.title}
+          description={data.description}
+          thumbnail={data.thumbnail}
+          facebook={data.facebook}
+          instagram={data.instagram}
+          linkedin={data.linkedin}
+          twitter={data.twitter}
+          inWebsite={data.inWebsite}
+          inMail={data.inMail}
+        />
+      </Container>
     )
   } else {
     return <div>Loading...</div>
