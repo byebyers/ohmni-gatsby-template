ff4febcee5c812a92bc263d38ef547871c8acd9a
Jacob Byers <byers.jacob@gmail.com>
2020-04-24 14:25:20 -0700

    Update: Layout added to team page

--- src/pages/team/index.md
+++ src/pages/team/index.md
@@ -1,3 +1,8 @@
+---
+templateKey: team-page
+path: /team
+---
+
 For me, art is about learning and about living with people. It's alive.
 
 ## \-Miuccia Prada
--- src/templates/team-page.js
+++ src/templates/team-page.js
@@ -1,11 +1,53 @@
 import React from "react"
+import PropTypes from 'prop-types'
 
-const TeamPage = ({ data }) => {
+// Components
+import { graphql } from 'gatsby'
+import Layout from '../components/layout/layout'
+import Container from '../components/container/container'
+import Content, { HTMLContent } from '../components/content/content'
+
+export const TeamPageTemplate = ({
+  body,
+  contentComponent,
+ }) => {
+  const PageContent = contentComponent || Content
   return (
-        <div>
-          test
-        </div>
+    <div>
+      <Container>
+          <PageContent className="content" content={body} />
+      </Container>
+    </div>
   )
 }
 
+TeamPageTemplate.propTypes = {
+  body: PropTypes.string,
+}
+
+const TeamPage = ({ data }) => {
+  const { markdownRemark: page } = data
+
+  return (
+    <Layout>
+      <TeamPageTemplate
+        contentComponent={HTMLContent}
+        body={page.html}
+      />
+    </Layout>
+  )
+}
+
+TeamPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
 export default TeamPage
+
+export const teamPageQuery = graphql`
+  query TeamPageQuery($id: String) {
+    markdownRemark(id: { eq: $id } frontmatter: {templateKey: {eq: "team-page"}, path: {eq: "/team"}}, fields: {}) {
+      html
+    }
+  }
+`
