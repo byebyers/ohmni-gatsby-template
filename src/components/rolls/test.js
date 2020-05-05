521fa1481f5cfe9f5ee2996c693150148bcb4690
Jacob Byers <byers.jacob@gmail.com>
2020-05-04 19:15:10 -0700

    Working with new rolls

--- src/components/rolls/test.js
+++ src/components/rolls/test.js
@@ -22,60 +22,67 @@ import './rolls.scss'
 
 const BlogRoll = ({ data, featured }) => {
   const { edges: posts } = data
+
+  const [appPosts, setAppPosts] = React.useState(posts);
+
+  const feature = event => {
+    setAppPosts(
+      featured ? posts.filter(post => post.featuredpost) : posts
+    )
+  }
+
   return (
     <div>
-      {posts &&
-        posts.map(({ node: post }) => (
+      {appPosts &&
+        appPosts.map(({ node: post }) => (
           <div>
-            {post.frontmatter.featured ? (
-              <article className="featured-post-container">
-                <div className="roll-post-content">
-                  <Link to={`/categories/category-${kebabCase(post.frontmatter.category)}/`} className="text-black">
-                    <span className="roll-category">{post.frontmatter.category}</span>
-                  </Link>
-                  <Link
-                    className="roll-title f-s"
-                    to={post.fields.slug}
-                  >
-                    {post.frontmatter.title}
-                  </Link>
-                  <Link
-                    className="roll-details roll-excerpt"
-                    to={post.fields.slug}
-                  >
-                    {post.excerpt}
-                  </Link>
-                  <div>
-                    <div className="roll-author">
-                      <Link to={`/authors/authors-${kebabCase(post.frontmatter.author)}/`} className="text-black">
-                        {post.frontmatter.author}
-                      </Link>
-                    </div>
-                    <div className="roll-details">
-                      <span>{post.frontmatter.date} ·&nbsp;</span>
-                      <span>{post.timeToRead} min read</span>
-                    </div>
+            <article className="featured-post-container">
+              <div className="roll-post-content">
+                <Link to={`/categories/category-${kebabCase(post.frontmatter.category)}/`} className="text-black">
+                  <span className="roll-category">{post.frontmatter.category}</span>
+                </Link>
+                <Link
+                  className="roll-title f-s"
+                  to={post.fields.slug}
+                >
+                  {post.frontmatter.title}
+                </Link>
+                <Link
+                  className="roll-details roll-excerpt"
+                  to={post.fields.slug}
+                >
+                  {post.excerpt}
+                </Link>
+                <div>
+                  <div className="roll-author">
+                    <Link to={`/authors/authors-${kebabCase(post.frontmatter.author)}/`} className="text-black">
+                      {post.frontmatter.author}
+                    </Link>
+                  </div>
+                  <div className="roll-details">
+                    <span>{post.frontmatter.date} ·&nbsp;</span>
+                    <span>{post.timeToRead} min read</span>
                   </div>
                 </div>
-                <div className="roll-image-container featured-image">
-                  {post.frontmatter.image ? (
+              </div>
+              <div className="roll-image-container featured-image">
+                {post.frontmatter.image ? (
 
-                      <Link
-                        className="roll-post-image"
-                        to={post.fields.slug}
-                      >
-                      <PreviewCompatibleImage
-                        imageInfo={{
-                          image: post.frontmatter.image,
-                          alt: `featured image for post ${post.frontmatter.title}`,
-                        }}
-                      />
-                      </Link>
+                    <Link
+                      className="roll-post-image"
+                      to={post.fields.slug}
+                    >
+                    <PreviewCompatibleImage
+                      imageInfo={{
+                        image: post.frontmatter.image,
+                        alt: `featured image for post ${post.frontmatter.title}`,
+                      }}
+                    />
+                    </Link>
 
-                  ) : null}
-                </div>
-              </article>
-            ) : null}
+                ) : null}
+              </div>
+            </article>
           </div>
         ))}
     </div>
